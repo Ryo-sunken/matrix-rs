d719406fda42318356828b21bd0aae401f63469c
Ryo-sunken <bellman.toratora@gmail.com>
2023-12-25 19:59:20 +0900

    演算子トレイト実装をモジュール分割

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 pub mod matrix;
+pub mod add;
+pub mod sub;
+pub mod matmul;
+pub mod scalarmul;
 
 #[cfg(test)]
 mod tests 
---
+++ src/scalarmul.rs
@@ -0,0 +1,152 @@
+use crate::matrix::Matrix;
+use std::ops::{Neg, Mul, MulAssign, Div, DivAssign};
+use rayon::prelude::*;
+
+macro_rules! defscalarmul {
+    ( $( $t: ty ),+ ) => {
+        $(
+            impl Mul<&Matrix<$t>> for $t
+            {
+                type Output = Matrix<$t>;
+
+                fn mul(self, rhs: &Matrix<$t>) -> Self::Output
+                {
+                    Self::Output
+                    {
+                        rows: rhs.rows,
+                        cols: rhs.cols,
+                        array: rhs.array.par_iter().map(|&x| x * self).collect(),
+                    }
+                }
+            }
+            impl Mul<Matrix<$t>> for $t
+            {
+                type Output = Matrix<$t>;
+
+                fn mul(self, rhs: Matrix<$t>) -> Self::Output
+                {
+                    self * &rhs
+                }
+            }
+        )+
+    };
+}
+
+
+impl<T> Neg for &Matrix<T>
+where
+    T: Neg + Copy + Send + Sync,
+    <T as Neg>::Output: Send + Sync,
+    Vec<T>: FromParallelIterator<<T as Neg>::Output>,
+{
+    type Output = Matrix<T>;
+
+    fn neg(self) -> Self::Output {
+        Self::Output
+        {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.par_iter()
+                .map(|&x| -x)
+                .collect(),
+        }
+    }
+}
+impl<T> Neg for Matrix<T>
+where
+    T: Neg + Copy + Send + Sync,
+    <T as Neg>::Output: Send + Sync,
+    Vec<T>: FromParallelIterator<<T as Neg>::Output>,
+{
+    type Output = Matrix<T>;
+
+    fn neg(self) -> Self::Output {
+        -&self
+    }
+}
+
+defscalarmul![i8, i16, i32, i64, isize, u8, u16, u32, u64, usize, f32, f64];
+
+impl<T> Mul<T> for &Matrix<T>
+where
+    T: Mul + Copy + Send + Sync,
+    <T as Mul>::Output: Send + Sync,
+    Vec<T>: FromParallelIterator<<T as Mul>::Output>,
+{
+    type Output = Matrix<T>;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Self::Output
+        {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.par_iter().map(|&x| x * rhs).collect(),
+        }
+    }
+}
+impl<T> Mul<T> for Matrix<T>
+where
+    T: Mul + Copy + Send + Sync,
+    <T as Mul>::Output: Send + Sync,
+    Vec<T>: FromParallelIterator<<T as Mul>::Output>,
+{
+    type Output = Matrix<T>;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        &self * rhs
+    }
+}
+
+impl<T> MulAssign<T> for Matrix<T>
+where
+    T: Mul + Copy + Send + Sync,
+    <T as Mul>::Output: Send + Sync,
+    Vec<T>: FromParallelIterator<<T as Mul>::Output>,
+{
+    fn mul_assign(&mut self, rhs: T) {
+        self.array = self.array.par_iter().map(|&x| x * rhs).collect();
+    }
+}
+
+
+
+impl<T> Div<T> for &Matrix<T>
+where
+    T: Div + Copy + Send + Sync,
+    <T as Div>::Output: Send + Sync,
+    Vec<T>: FromParallelIterator<<T as Div>::Output>,
+{
+    type Output = Matrix<T>;
+
+    fn div(self, rhs: T) -> Self::Output {
+        Self::Output
+        {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.par_iter().map(|&x| x / rhs).collect(),
+        }
+    }
+}
+impl<T> Div<T> for Matrix<T>
+where
+    T: Div + Copy + Send + Sync,
+    <T as Div>::Output: Send + Sync,
+    Vec<T>: FromParallelIterator<<T as Div>::Output>,
+{
+    type Output = Matrix<T>;
+
+    fn div(self, rhs: T) -> Self::Output {
+        &self / rhs
+    }
+}
+
+impl<T> DivAssign<T> for Matrix<T>
+where
+    T: Div + Copy + Send + Sync,
+    <T as Div>::Output: Send + Sync,
+    Vec<T>: FromParallelIterator<<T as Div>::Output>,
+{
+    fn div_assign(&mut self, rhs: T) {
+        self.array = self.array.par_iter().map(|&x| x / rhs).collect()
+    }
+}
