bdf312f5859f34edc049e076677465a861a4634b
Ryo-sunken <bellman.toratora@gmail.com>
2023-12-25 23:26:56 +0900

    rayonによる並列化

--- src/ops/sum.rs
+++ src/ops/sum.rs
@@ -4,30 +4,24 @@ use rayon::prelude::*;
 
 impl<T> Matrix<T>
 where
-    T: Sum + Copy + Send + Sync,
+    T: Sum + for<'a> Sum<&'a T> + Copy + Send + Sync,
 {
     pub fn sum(&self, ax: Axis) -> Self
     {
         match ax {
             Axis::ROW => {
-                let split_array = self.array.chunks(self.cols)
-                    .map(|s| s.into())
-                    .collect::<Vec<Vec<T>>>();
-                let sum_array = split_array.into_par_iter()
-                    .map(|s| s.into_iter().sum())
+                let sum_array = self.array.par_chunks(self.cols)
+                    .map(|s| s.iter().sum())
                     .collect::<Vec<T>>();
                 Self::from_vec_col(sum_array)
             }
             Axis::COLUMN => {
-                let split_array = self.transpose().array.chunks(self.rows)
-                    .map(|s| s.into())
-                    .collect::<Vec<Vec<T>>>();
-                let sum_array = split_array.into_par_iter()
-                    .map(|s| s.into_iter().sum())
+                let sum_array = self.transpose().array.par_chunks(self.rows)
+                    .map(|s| s.iter().sum())
                     .collect::<Vec<T>>();
                 Self::from_vec_row(sum_array)
             }
-            Axis::BOTH => Self::new([[self.array.clone().into_par_iter().sum()]])
+            Axis::BOTH => Self::new([[self.array.par_iter().sum()]])
         }
     }
 }
