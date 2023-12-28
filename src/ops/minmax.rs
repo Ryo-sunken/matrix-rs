f7dbb38cba21184e16a4b269c1c5b455b8820113
Ryo-sunken <bellman.toratora@gmail.com>
2023-12-28 15:08:04 +0900

    min maxのジェネリクス化

--- src/ops/minmax.rs
+++ src/ops/minmax.rs
@@ -1,22 +1,21 @@
 use crate::matrix::{Matrix, Axis};
-use num_traits::Float;
 use rayon::prelude::*;
 
 impl<T> Matrix<T>
 where
-    T: Float + Copy + Send + Sync,
+    T: PartialOrd + Copy + Send + Sync,
 {
     fn max_array(matrix: &Matrix<T>) -> Vec<T>
     {
         matrix.array.par_chunks(matrix.cols)
-            .map(|s| s.iter().fold(T::neg_infinity(), |x, &y| x.max(y)))
+            .map(|s| *s.iter().reduce(|x, y| if x > y { x } else { y }).unwrap())
             .collect::<Vec<_>>()
     }
 
     fn min_array(matrix: &Matrix<T>) -> Vec<T>
     {
         matrix.array.par_chunks(matrix.cols)
-            .map(|s| s.iter().fold(T::infinity(), |x, &y| x.min(y)))
+            .map(|s| *s.iter().reduce(|x, y| if x > y { y } else { x }).unwrap())
             .collect::<Vec<_>>()
     }
 
@@ -25,7 +24,7 @@ where
         match ax {
             Axis::ROW => Self::from_vec_col(Self::max_array(self)),
             Axis::COLUMN => Self::from_vec_row(Self::max_array(&self.transpose())),
-            Axis::BOTH => Self::new([[self.array.iter().fold(T::neg_infinity(), |x, &y| x.max(y))]]),
+            Axis::BOTH => Self::new([[*self.array.iter().reduce(|x, y| if x > y { x } else { y }).unwrap()]]),
         }
     }
 
@@ -34,8 +33,7 @@ where
         match ax {
             Axis::ROW => Self::from_vec_col(Self::min_array(self)),
             Axis::COLUMN => Self::from_vec_row(Self::min_array(&self.transpose())),
-            Axis::BOTH => Self::new([[self.array.iter().fold(T::infinity(), |x, &y| x.min(y))]]),
+            Axis::BOTH => Self::new([[*self.array.iter().reduce(|x, y| if x > y { y } else { x }).unwrap()]]),
         }
     }
 }
-
