f4c6c1fe708e00da9bf14a380ab157b345fbc638
Ryo-sunken <bellman.toratora@gmail.com>
2023-12-28 14:16:56 +0900

    min maxのジェネリクス化

--- src/ops/minmax.rs
+++ src/ops/minmax.rs
@@ -1,45 +1,41 @@
 use crate::matrix::{Matrix, Axis};
+use num_traits::Float;
 use rayon::prelude::*;
 
-impl Matrix<f64>
+impl<T> Matrix<T>
+where
+    T: Float + Copy + Send + Sync,
 {
+    fn max_array(matrix: &Matrix<T>) -> Vec<T>
+    {
+        matrix.array.par_chunks(matrix.cols)
+            .map(|s| s.iter().fold(T::neg_infinity(), |x, &y| x.max(y)))
+            .collect::<Vec<_>>()
+    }
+
+    fn min_array(matrix: &Matrix<T>) -> Vec<T>
+    {
+        matrix.array.par_chunks(matrix.cols)
+            .map(|s| s.iter().fold(T::infinity(), |x, &y| x.min(y)))
+            .collect::<Vec<_>>()
+    }
+
     pub fn max(&self, ax: Axis) -> Self
     {
-        let max = |x, &y| if y > x { y } else { x };
         match ax {
-            Axis::ROW => {
-                let max_array = self.array.par_chunks(self.cols)
-                    .map(|s| s.iter().fold(f64::NEG_INFINITY, max))
-                    .collect::<Vec<_>>();
-                Self::from_vec_col(max_array)
-            },
-            Axis::COLUMN => {
-                let max_array = self.transpose().array.par_chunks(self.rows)
-                    .map(|s| s.iter().fold(f64::NEG_INFINITY, max))
-                    .collect::<Vec<_>>();
-                Self::from_vec_row(max_array)
-            },
-            Axis::BOTH => Self::new([[self.array.iter().fold(f64::NEG_INFINITY, max)]]),
+            Axis::ROW => Self::from_vec_col(Self::max_array(self)),
+            Axis::COLUMN => Self::from_vec_row(Self::max_array(&self.transpose())),
+            Axis::BOTH => Self::new([[self.array.iter().fold(T::neg_infinity(), |x, &y| x.max(y))]]),
         }
     }
 
     pub fn min(&self, ax: Axis) -> Self
     {
-        let min = |x, &y| if y < x { y } else { x };
         match ax {
-            Axis::ROW => {
-                let min_array = self.array.par_chunks(self.cols)
-                    .map(|s| s.iter().fold(f64::INFINITY, min))
-                    .collect::<Vec<_>>();
-                Self::from_vec_col(min_array)
-            },
-            Axis::COLUMN => {
-                let min_array = self.transpose().array.par_chunks(self.rows)
-                    .map(|s| s.iter().fold(f64::INFINITY, min))
-                    .collect::<Vec<_>>();
-                Self::from_vec_row(min_array)
-            },
-            Axis::BOTH => Self::new([[self.array.iter().fold(f64::INFINITY, min)]]),
+            Axis::ROW => Self::from_vec_col(Self::min_array(self)),
+            Axis::COLUMN => Self::from_vec_row(Self::min_array(&self.transpose())),
+            Axis::BOTH => Self::new([[self.array.iter().fold(T::infinity(), |x, &y| x.min(y))]]),
         }
     }
 }
+
