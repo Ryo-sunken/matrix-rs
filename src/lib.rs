656fd03546dbc5a54482ecb1ecdfd754e567053f
Ryo-sunken <bellman.toratora@gmail.com>
2024-01-02 00:47:30 +0900

    テストの追加

--- src/lib.rs
+++ src/lib.rs
@@ -100,6 +100,26 @@ mod tests {
     }
 
     #[test]
+    fn cwise_max() {
+        let x = Matrix::new([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]]);
+        let y = Matrix::new([[9., 8., 7.], [6., 5., 4.], [3., 2., 1.]]);
+        assert_eq!(
+            x.cwise_max(&y),
+            Matrix::new([[9., 8., 7.], [6., 5., 6.], [7., 8., 9.]])
+        );
+    }
+
+    #[test]
+    fn cwise_min() {
+        let x = Matrix::new([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]]);
+        let y = Matrix::new([[9., 8., 7.], [6., 5., 4.], [3., 2., 1.]]);
+        assert_eq!(
+            x.cwise_min(&y),
+            Matrix::new([[1., 2., 3.], [4., 5., 4.], [3., 2., 1.]])
+        );
+    }
+
+    #[test]
     fn matmul() {
         let x = Matrix::new([[1., 2., 3.], [4., 5., 6.]]);
         let y = Matrix::new([[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.]]);
