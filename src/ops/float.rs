0f429ba492aec4cfffcbc2a29f77ee7a72d0f436
Ryo-sunken <bellman.toratora@gmail.com>
2024-01-02 00:54:40 +0900

    float用関数の実装

--- src/ops/float.rs
+++ src/ops/float.rs
@@ -2,11 +2,147 @@ use crate::matrix::Matrix;
 use num_traits::Float;
 
 impl<T: Float> Matrix<T> {
+    pub fn floor(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.floor()).collect(),
+        }
+    }
+
+    pub fn ceil(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.ceil()).collect(),
+        }
+    }
+
+    pub fn round(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.round()).collect(),
+        }
+    }
+
+    pub fn trunc(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.trunc()).collect(),
+        }
+    }
+
+    pub fn fract(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.fract()).collect(),
+        }
+    }
+
+    pub fn abs(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.abs()).collect(),
+        }
+    }
+
+    pub fn signum(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.signum()).collect(),
+        }
+    }
+
+    pub fn recip(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.recip()).collect(),
+        }
+    }
+
+    pub fn powi(&self, n: i32) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.powi(n)).collect(),
+        }
+    }
+
+    pub fn powf(&self, n: T) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.powf(n)).collect(),
+        }
+    }
+
+    pub fn sqrt(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.sqrt()).collect(),
+        }
+    }
+
     pub fn exp(&self) -> Self {
         Self {
             rows: self.rows,
             cols: self.cols,
             array: self.array.iter().map(|&x| x.exp()).collect(),
+        }
+    }
+
+    pub fn exp2(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.exp2()).collect(),
+        }
+    }
+
+    pub fn ln(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.ln()).collect(),
+        }
+    }
+
+    pub fn log(&self, base: T) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.log(base)).collect(),
+        }
+    }
+
+    pub fn log2(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.log2()).collect(),
+        }
+    }
+
+    pub fn log10(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.log10()).collect(),
+        }
+    }
+
+    pub fn cbrt(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.cbrt()).collect(),
         }
     }
 
@@ -26,19 +162,35 @@ impl<T: Float> Matrix<T> {
         }
     }
 
-    pub fn sqrt(&self) -> Self {
+    pub fn tan(&self) -> Self {
         Self {
             rows: self.rows,
             cols: self.cols,
-            array: self.array.iter().map(|&x| x.sqrt()).collect(),
+            array: self.array.iter().map(|&x| x.tan()).collect(),
         }
     }
 
-    pub fn powf(&self, n: T) -> Self {
+    pub fn sinh(&self) -> Self {
         Self {
             rows: self.rows,
             cols: self.cols,
-            array: self.array.iter().map(|&x| x.powf(n)).collect(),
+            array: self.array.iter().map(|&x| x.sinh()).collect(),
+        }
+    }
+
+    pub fn cosh(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.cosh()).collect(),
+        }
+    }
+
+    pub fn tanh(&self) -> Self {
+        Self {
+            rows: self.rows,
+            cols: self.cols,
+            array: self.array.iter().map(|&x| x.tanh()).collect(),
         }
     }
 }
